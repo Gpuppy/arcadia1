dcd3f1e77e171371ae01530a4fce56742eb29980
Gael Adam <gaelswim@hotmail.com>
2024-11-10 14:38:56 +0100

    .env path changed

--- connection.php
+++ connection.php
@@ -4,6 +4,10 @@
 require_once "config/DbConnection.php";
 require_once 'config/session.php';
 require_once "templates/header.php";
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv -> load();
 
 //echo $_SESSION['user']['username'];
 

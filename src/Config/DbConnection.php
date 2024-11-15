5abffb3b904b7ae48ae4c3659da0319e173d3e84
Gael Adam <gaelswim@hotmail.com>
2024-11-15 20:48:59 +0100

    updated index.php

--- src/Config/DbConnection.php
+++ src/Config/DbConnection.php
@@ -28,6 +28,20 @@ $query_builder = TRUE;
 $conn = mysqli_connect($jawsdbhost, $jawsdbuser, $jawsdbpassword, $jawsdbname);
 
 
+//Get Heroku ClearDB connection information
+/*$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>*/
+
+
+
 try {
     $pdo = new \PDO("mysql:$jawsdbhost;dbname:$jawsdbname", $jawsdbuser, $jawsdbpassword);
     echo " Connexion à la base de données";

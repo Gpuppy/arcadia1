9dcb4fdbcac405b8f0d7dc8596f5476be8c87109
Gael Adam <gaelswim@hotmail.com>
2025-05-09 16:00:44 +0200

    contact-form

--- src/contact.php
+++ src/contact.php
@@ -8,10 +8,22 @@ require_once __DIR__ . '/../src/Config/DbConnection.php';
 require_once "../templates/header.php";
 
 $db = new DbConnection();
-$version = '';
+//$version = '';
 
 
 ?>
+<body>
+<form class="contact-form" action="contact.php" method="post">
+    <div class="row">
+        Name: <input type="text" name="name" value="">
+        Email: <input type="text" name="email" value="">
+        Sujet: <input type="text" name="subject" value="">
+        Message: <textarea name="message" rows="5" ></textarea>
+        <button type="submit" class="btn btn-success">Envoyer</button>
+    </div>
+ </form>
+</body>
+
 <?php
 require "../templates/footer.php";
 ?>

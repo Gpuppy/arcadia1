2a02aa6f6ad95cd35116b345cdeb2b1cfe04d5c1
Gael Adam <gaelswim@hotmail.com>
2025-05-19 21:44:30 +0200

    src/contact

--- src/contact_form.php
+++ src/contact_form.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -7,63 +9,70 @@ use PHPMailer\PHPMailer\Exception;
 
 
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+/*if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $_POST['name'] ?? '';
     $emailFrom = $_POST['email'] ?? '';
     $subject = $_POST['subject'] ?? '';
-    $message = $_POST['message'] ?? '';
+    $message = $_POST['message'] ?? '';*/
 
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$subject = $_POST['subject'] ?? '';
+$message = $_POST['message'] ?? '';
+
+    $old = compact('name', 'email', 'subject','message');
     $errors = [];
-    $old = $_POST;
 
     if (empty($name)) {
-        $errors [] = "Le nom est necessaire. ";
+        $errors ['name'] = "Le nom est necessaire. ";
         }
 
-    if (empty($emailFrom) || !filter_var($emailFrom, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Un email valide est requis.";
+    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Un email valide est requis.";
     }
 
     if (empty($subject)) {
-        $errors[] = "le sujet est requis.";
+        $errors['subject'] = "le sujet est requis.";
     }
 
     if (empty($message)) {
-        $errors[] = "le message ne peux étre vide.";
+        $errors['message'] = "le message ne peux étre vide.";
     }
 
-    if (empty($errors)) {
-
+    if (!empty($errors)) {
+        $_SESSION['errors'] = $errors;
+        $_SESSION['old'] = $old;
+        header('Location:contact.php');
+        exit;
+    }
         $mail = new PHPMailer(true);
 
         try {
             $mail->isSMTP();
             $mail->SMTPAuth = true;
-
             $mail->Host = "smtp.gmail.com";
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
-
             $mail->Username = 'gadetreg@gmail.com';
             $mail->Password = 'nnqyhfhguxelwplc';
-
             $mail->setFrom('gadetreg@gmail.com', 'Arcadia');
             $mail->addAddress("gadetreg@gmail.com", "Gael");
-            $mail->addReplyTo($emailFrom, $name);  // ← the user's email
-
+            $mail->addReplyTo($email, $name);  // ← the user's email
             $mail->Subject = $subject;
-            $mail->Body = "Message from $name ($emailFrom):\n\n$message";
+            $mail->Body = "Message from $name ($email):\n\n$message";
 
             $mail->send();
-            echo header('Location:email_sent.php');
+
+            header('Location:email_sent.php');
+
         } catch (Exception $e) {
             echo "Erreur lors de l'envoi: {$mail->ErrorInfo}";
-        }
-    } else {
+
+    } /*else {
         // Show validation errors
         echo implode("<br>", $errors);
-    }
-}
+    }*/
+
 
 
 
